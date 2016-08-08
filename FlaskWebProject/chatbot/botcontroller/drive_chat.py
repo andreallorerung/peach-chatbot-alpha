fedec76627b5f69c0bc30479be109f81c84afc12
nterreri <niccolo.terreri@gmail.com>
2016-08-08 12:53:50 +0100

    continuing work on conversation driver

--- FlaskWebProject/chatbot/botcontroller/drive_chat.py
+++ FlaskWebProject/chatbot/botcontroller/drive_chat.py
@@ -1,41 +1,86 @@
 '''Module to drive the chatbot brain through the highlighted topics'''
+import operator
+import topics
 import concerns
 
 class ConversationDriver(object):
     '''Class to drive the conversation through the questionnaire topics'''
-    def __init__(self, rivescriptInterpreter):
-        self.rivescriptInterpreter = rivescriptInterpreter
+    def __init__(self, userid):
+        self.userid = userid
+        self.sortedUserConcernsNames = self._sortUserConcerns()
 
-    def setIssuesForUser(self, userid, issues):
-        for issue in issues:
-            self._setIssueForUser(userid, *issue) #each issue is a pair
+    def setInitialUserConcerns(self, concerns):
+        for concern in concerns:
+            self._setConcern(*concern) #each concern is a pair
+        self.sortedUserConcernNames = self._sortUserConcerns()
 
-    def _setIssueForUser(self, userid, issueName, issueScore):
-        '''Sets an issue score for the current user'''
-        concerns = concerns.ConcernsFactory.getConcerns(userid)
-        concerns[issueName] = issueScore
+    def _setConcern(self, concernName, concernScore):
+        '''Sets an concern score for the current user'''
+        userConcerns = concerns.UserConcernsFactory.getUserConcerns(self.userid)
+        userConcerns[concernName] = concerns.Concern(concernScore)
 
-    def reply(self, message):
-        return "hello"
-        # initialized: issue set to most distressful
+    def _sortUserConcerns(self):
+        unsortedUserConcerns = self._getUserConcernsAsCoupleOfNameAndScore(self.userid)
+        sortedUserConcerns = self._sortConcernsByDistressScore(unsortedUserConcerns)
 
-        # if all issues have been discussed finalize user session,
+        sortedUserConcernNames = []
+        for concern in sortedUserConcerns:
+            sortedUserConcernNames.append(concern[0])
+
+        return sortedUserConcernNames
+
+    @staticmethod
+    def _getUserConcernsAsCoupleOfNameAndScore(userid):
+        userConcerns = concerns.UserConcernsFactory.getUserConcerns(userid)
+        unsortedUserConcerns = []
+
+        for concernName in topics.ALL_TOPICS:
+            concern = userConcerns[concernName]
+            if concern is not None:
+                distressScore = concern.getDistressScore()
+                unsortedUserConcerns.append( (concernName, distressScore) )
+        return unsortedUserConcerns
+
+    def _sortConcernsByDistressScore(self, unsortedUserConcerns):
+        return sorted(unsortedUserConcerns, key=operator.itemgetter(1), reverse=True)
+
+    # def reply(self, message):
+    #     return "hello"
+        # initialized: concern set to most distressful
+        #
+        # if all concerns have been discussed, then finalize user session,
         # by asking the user to terminate conversation, user can at
         # this point only ask for a specific topic or agree to
         # ending conversation
-
+        #
         # loop body:
         # set topic in interpreter
         # forward preprocessed message to interpreter
         # get reply and return reply
         # interpreter can ask permission to change topic
         # in which case mark current topic as discussed
-
+        #
         # when is discussion saved? discussion must be saved one level
         # above in order to have access to unprocessed user input
 
-    def moveToNextIssue(self):
+    def moveToNextConcern(self):
         pass
 
-    def hasTopicBeenCovered(self):
-        pass
+    def concernHasBeenAddressed(self, concernName):
+        userConcerns = concerns.UserConcernsFactory.getUserConcerns(self.userid)
+        concern = userConcerns[concernName]
+
+        return (concern is not None) and concern.hasBeenAddressed()
+
+    def _markAddressed(self, concernName):
+        concern = self._getUserConcern(concernName)
+        concern.setAddressed()
+
+    def _getUserConcern(self, concernName):
+        userConcerns = concerns.UserConcernsFactory.getUserConcerns(self.userid)
+        concern = userConcerns[concernName]
+
+        if concern is None:
+            raise KeyError("No concern about '{}' for userid '{}'"\
+                            .format(concernName, self.userid))
+        else: return concern
--- FlaskWebProject/chatbot/tests/tests_unit/test_botcontroller/test_drive.py
+++ FlaskWebProject/chatbot/tests/tests_unit/test_botcontroller/test_drive.py
@@ -1,26 +1,26 @@
+import pytest
 import collections
 from rivescript import RiveScript
 from botcontroller.drive_chat import ConversationDriver
-from botcontroller.concerns import ConcernsFactory
+from botcontroller.concerns import UserConcernsFactory
 from botinterface.message import Message
 
-conversationDriver = ConversationDriver(RiveScript())
-someuserid = "toby"
-def test_hasrivescript():
-    assert hasattr(conversationDriver, "rivescriptInterpreter")
+currentuserid = "toby"
+conversationDriver = ConversationDriver(currentuserid)
 
-def test_rivescriptInterpreterIsRivescript():
-    assert RiveScript is type(conversationDriver.rivescriptInterpreter)
+def test_hasuserid():
+    attributeName = "userid"
+    assert hasattr(conversationDriver, attributeName)
 
-def test_reply():
-    message = "Hello"
+# def test_reply():
+#     message = "Hello"
+#
+#     replied = conversationDriver.reply(message)
+#
+#     assert replied is not None
 
-    replied = conversationDriver.reply(message)
-
-    assert replied is not None
-
-def test_setissuesforuser():
-    issues = [("respiratory", 5),
+def test_setinitialconcerns():
+    concerns = [("respiratory", 5),
             ("urinary",  8),
             ("sleeping", 2),
             ("chores", 1),
@@ -31,21 +31,58 @@ def test_setissuesforuser():
             ("regret", 9),
             ("partner", 3)]
 
-    someuserid = "toby"
+    currentuserid = "toby"
 
-    conversationDriver.setIssuesForUser(someuserid, issues)
+    conversationDriver.setInitialUserConcerns(concerns)
 
-    assert theseConcernsHaveBeenStoredForTheUser(issues, someuserid)
+    assert theseUserConcernsHaveBeenStoredForTheUser(concerns, currentuserid)
 
-def theseConcernsHaveBeenStoredForTheUser(issues, userid):
-    for issue in issues:
-        if not issueScoreHasBeenStoredForUser(issue, userid):
+def theseUserConcernsHaveBeenStoredForTheUser(concerns, currentuserid):
+    for concern in concerns:
+        if not concernScoreHasBeenStoredForUser(concern, currentuserid):
             return False
 
     return True
 
-def issueScoreHasBeenStoredForUser(issue, userid):
-        userConcers = ConcernsFactory.getConcerns(userid)
-        issueName = issue[0]
-        issueScore = issue[1]
-        return userConcers[issueName] == issueScore
+def concernScoreHasBeenStoredForUser(concern, currentuserid):
+        userConcers = UserConcernsFactory.getUserConcerns(currentuserid)
+        concernName = concern[0]
+        expectedConcernScore = concern[1]
+
+        concern = userConcers[concernName]
+        actualConcernScore = concern.getDistressScore()
+        return ( expectedConcernScore == actualConcernScore )
+
+def test_concernsHaveBeenSorted():
+    sortedConcernScores = getConcernScoresByNames(currentuserid, conversationDriver.sortedUserConcernNames)
+
+    expectedOrderedList = sorted(sortedConcernScores, reverse=True)
+    assert expectedOrderedList == sortedConcernScores #asser that list is sorted in descending order
+
+def getConcernScoresByNames(userid, namesOfConcerns):
+    userConcerns = UserConcernsFactory.getUserConcerns(userid)
+    concernScores = []
+
+    for concernName in namesOfConcerns:
+        concern = userConcerns[concernName]
+        distressScore = concern.getDistressScore()
+
+        concernScores.append(distressScore)
+
+    return concernScores
+
+def test_concernhasbeenaddressed():
+    concernName = "sleeping"
+    conversationDriver._markAddressed(concernName)
+
+    assert conversationDriver.concernHasBeenAddressed(concernName)
+
+def test_concernhasbeenaddressed_noconcern():
+    concernName = "yawning"
+
+    with pytest.raises(KeyError):
+        conversationDriver._markAddressed(concernName)
+
+def test_not_concernhasbeenaddressedforuser():
+    concernName = "respiratory"
+    assert not conversationDriver.concernHasBeenAddressed(concernName)
