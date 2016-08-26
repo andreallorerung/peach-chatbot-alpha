332b2d817661e0148bb66881991628296ca87c8b
nterreri <niccolo.terreri@gmail.com>
2016-08-26 23:42:41 +0100

    fixed import name

--- FlaskWebProject/chatbot/preprocess/preprocessor_builder.py
+++ FlaskWebProject/chatbot/preprocess/preprocessor_builder.py
@@ -1,6 +1,6 @@
 import preprocess.tokenizer_simple
 import preprocess.stopwords_remover_lenient
-import preprocess.stemmer_factory
+import preprocess.stemming_lancaster
 import botinterface.preprocessor
 
 def build():
