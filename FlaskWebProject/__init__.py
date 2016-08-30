b268582342b627c856ac08d2a782cf9493d1fc15
dmw2 <debswacks@yahoo.com>
2016-08-30 14:22:38 +0100

    cleaning up views.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -4,8 +4,10 @@ The flask application package.
 
 from flask import Flask
 import os
+import sys
 
-sys.path.append("FlaskWebProject/chatbot")
+
+
 
 app = Flask(__name__)
 app.secret_key=os.urandom(24)
