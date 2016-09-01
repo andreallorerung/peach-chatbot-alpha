1a29f1a35949a961bba12d70e6b2cd55857021f1
nterreri <niccolo.terreri@gmail.com>
2016-09-01 22:40:38 +0100

    changed import statement again

--- FlaskWebProject/chatbot/postprocess/postprocessor_builder.py
+++ FlaskWebProject/chatbot/postprocess/postprocessor_builder.py
@@ -3,6 +3,6 @@ import message_decorator_single
 
 def build():
     return None
-    # botinterface.postprocessor.MessagePostprocessor(keywordExtractor=postprocess.keyword_extractor_single.SingleKeywordExtractor(),
+    # postprocessor.MessagePostprocessor(keywordExtractor=postprocess.keyword_extractor_single.SingleKeywordExtractor(),
     #                                                         searchAdapter= the real search adapter,
     #                                                         messageDecorator=postprocess.message_decorator_single.MessageDecoratorSingle())
