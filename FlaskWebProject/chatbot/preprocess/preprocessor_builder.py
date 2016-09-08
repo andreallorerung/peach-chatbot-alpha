076b58b6f09ecccaea8a39e777c50d9003cf8b57
nterreri <niccolo.terreri@gmail.com>
2016-09-08 20:35:56 +0100

    preprocessor builder is the problem, made it a dummy

--- FlaskWebProject/chatbot/preprocess/preprocessor_builder.py
+++ FlaskWebProject/chatbot/preprocess/preprocessor_builder.py
@@ -1,9 +1,10 @@
-import tokenizer_simple
-import stopwords_remover_lenient
-import stemming_lancaster
-import preprocessor
+# import tokenizer_simple
+# import stopwords_remover_lenient
+# import stemming_lancaster
+# import preprocessor
 
 def build():
-    return preprocessor.MessagePreprocessor(tokenizer=tokenizer_simple.SimpleTokenizerProxy(),
-                                        stopwordRemover=stopwords_remover_lenient.StopwordRemoverLenient(),
-                                        stemmer=stemming_lancaster.Lancaster())
+    pass
+    # return preprocessor.MessagePreprocessor(tokenizer=tokenizer_simple.SimpleTokenizerProxy(),
+    #                                     stopwordRemover=stopwords_remover_lenient.StopwordRemoverLenient(),
+    #                                     stemmer=stemming_lancaster.Lancaster())
