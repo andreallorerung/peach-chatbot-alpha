ddf4f05f9bf7d53926ba91362acbf20eea9fbca5
nterreri <niccolo.terreri@gmail.com>
2016-09-08 20:47:27 +0100

    restored chatbot in views.py

--- FlaskWebProject/chatbot/preprocess/preprocessor_builder.py
+++ FlaskWebProject/chatbot/preprocess/preprocessor_builder.py
@@ -4,7 +4,6 @@ import stemming_lancaster
 import preprocessor
 
 def build():
-    pass
     return preprocessor.MessagePreprocessor(tokenizer=tokenizer_simple.SimpleTokenizerProxy(),
                                         stopwordRemover=stopwords_remover_lenient.StopwordRemoverLenient(),
                                         stemmer=stemming_lancaster.Lancaster())
