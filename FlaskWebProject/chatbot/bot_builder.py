12342999df8c7a399baddc4433e6b3b231aac95e
nterreri <niccolo.terreri@gmail.com>
2016-09-08 20:42:19 +0100

    restoring everything

--- FlaskWebProject/chatbot/bot_builder.py
+++ FlaskWebProject/chatbot/bot_builder.py
@@ -5,42 +5,41 @@ import preprocess.preprocessor_builder
 import postprocess.postprocessor_builder
 
 def build():
-    pass
-#     preprocessor = preprocess.preprocessor_builder.build()
-#     postprocessor = postprocess.postprocessor_builder.build()
-#     interpreter = botinterface.rivescript_proxy.RiveScriptProxy(\
-#                         rivescriptInterpreter=rivescript.RiveScript(debug=False))
-#     return botinterface.bot_rivescript.BotRivescript(preprocessor=preprocessor,
-#                                 interpreter=interpreter,
-#                                 postprocessor=postprocessor)
-#
-# class BotBuilder(object):
-#
-#     def __init__(self):
-#         self.preprocessor = preprocess.preprocessor_builder.build()
-#         self.postprocessor = postprocess.postprocessor_builder.build()
-#         self.interpreter = None
-#         self.brain = "./brain"
-#
-#     def addBrain(self, brain):
-#         self.brain = brain
-#
-#     def addPreprocessor(self, preprocessor):
-#         self.preprocessor = preprocessor
-#
-#     def addPostprocessor(self, postprocessor):
-#         self.postprocessor = postprocessor
-#
-#     def addInterpreter(self, interpreter):
-#         self.interpreter = interpreter
-#
-#     def build(self):
-#         if self.interpreter is None:
-#             productionRiveScript = rivescript.RiveScript(debug=False)
-#             self.interpreter = botinterface.rivescript_proxy.RiveScriptProxy(
-#                 rivescriptInterpreter=productionRiveScript, brain=self.brain)
-#
-#         return botinterface.bot_rivescript.BotRivescript(
-#             preprocessor=self.preprocessor,
-#             postprocessor=self.postprocessor,
-#             interpreter=self.interpreter)
+    preprocessor = preprocess.preprocessor_builder.build()
+    postprocessor = postprocess.postprocessor_builder.build()
+    interpreter = botinterface.rivescript_proxy.RiveScriptProxy(\
+                        rivescriptInterpreter=rivescript.RiveScript(debug=False))
+    return botinterface.bot_rivescript.BotRivescript(preprocessor=preprocessor,
+                                interpreter=interpreter,
+                                postprocessor=postprocessor)
+
+class BotBuilder(object):
+
+    def __init__(self):
+        self.preprocessor = preprocess.preprocessor_builder.build()
+        self.postprocessor = postprocess.postprocessor_builder.build()
+        self.interpreter = None
+        self.brain = "./brain"
+
+    def addBrain(self, brain):
+        self.brain = brain
+
+    def addPreprocessor(self, preprocessor):
+        self.preprocessor = preprocessor
+
+    def addPostprocessor(self, postprocessor):
+        self.postprocessor = postprocessor
+
+    def addInterpreter(self, interpreter):
+        self.interpreter = interpreter
+
+    def build(self):
+        if self.interpreter is None:
+            productionRiveScript = rivescript.RiveScript(debug=False)
+            self.interpreter = botinterface.rivescript_proxy.RiveScriptProxy(
+                rivescriptInterpreter=productionRiveScript, brain=self.brain)
+
+        return botinterface.bot_rivescript.BotRivescript(
+            preprocessor=self.preprocessor,
+            postprocessor=self.postprocessor,
+            interpreter=self.interpreter)
--- FlaskWebProject/chatbot/preprocess/preprocessor_builder.py
+++ FlaskWebProject/chatbot/preprocess/preprocessor_builder.py
@@ -1,10 +1,10 @@
 import tokenizer_simple
-# import stopwords_remover_lenient
-# import stemming_lancaster
-# import preprocessor
+import stopwords_remover_lenient
+import stemming_lancaster
+import preprocessor
 
 def build():
     pass
-    # return preprocessor.MessagePreprocessor(tokenizer=tokenizer_simple.SimpleTokenizerProxy(),
-    #                                     stopwordRemover=stopwords_remover_lenient.StopwordRemoverLenient(),
-    #                                     stemmer=stemming_lancaster.Lancaster())
+    return preprocessor.MessagePreprocessor(tokenizer=tokenizer_simple.SimpleTokenizerProxy(),
+                                        stopwordRemover=stopwords_remover_lenient.StopwordRemoverLenient(),
+                                        stemmer=stemming_lancaster.Lancaster())
